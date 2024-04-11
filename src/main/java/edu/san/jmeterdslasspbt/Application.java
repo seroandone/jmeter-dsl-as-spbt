2875fedef4b582b4a6f7b928ffac1e7e78976ce3
Sankar D <san.gct@gmail.com>
2024-04-11 00:35:08 -0400

    first commit

---
+++ src/main/java/edu/san/jmeterdslasspbt/Application.java
@@ -0,0 +1,13 @@
+package edu.san.jmeterdslasspbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterDslAsSpbtApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JmeterDslAsSpbtApplication.class, args);
+  }
+
+}
