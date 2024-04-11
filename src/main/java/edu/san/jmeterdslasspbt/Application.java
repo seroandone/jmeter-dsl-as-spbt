1cb9d74fff4c4f8ce5b4ed2b51eb497907f21442
Sankar D <san.gct@gmail.com>
2024-04-11 00:35:28 -0400

    first commit

--- src/main/java/edu/san/jmeterdslasspbt/Application.java
+++ src/main/java/edu/san/jmeterdslasspbt/Application.java
@@ -1,13 +1,28 @@
 package edu.san.jmeterdslasspbt;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import us.abstracta.jmeter.javadsl.core.TestPlanStats;
+
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+
 
 @SpringBootApplication
-public class JmeterDslAsSpbtApplication {
+public class Application implements CommandLineRunner {
 
   public static void main(String[] args) {
-    SpringApplication.run(JmeterDslAsSpbtApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("Hello World!");
+
+    TestPlanStats stats = testPlan(
+        threadGroup(2, 10,
+            httpSampler("https://abstracta.github.io/jmeter-java-dsl/")
+        )
+    ).run();
+  }
 }
